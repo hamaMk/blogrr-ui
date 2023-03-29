1d607d8c8b4d239fe2c3dfb2042351e5c782b743
hamaMk <hamandishemk2014@gmail.com>
2023-03-29 14:56:43 +0200

    feat: Add layout to the app
    Added a default layout for header, footer reuse.

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-layout>
+      <!-- Header -->
+      <v-app-bar
+          color="teal-darken-4"
+          image="https://picsum.photos/1920/1080?random"
+      >
+        <template v-slot:image>
+          <v-img
+              gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
+
+        <v-app-bar-title>Blogrr</v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+      </v-app-bar>
+
+      <!-- Content -->
+      <v-main>
+        <slot/>
+      </v-main>
+
+      <!-- Footer -->
+    </v-layout>
+    <footer class="px-10 py-5 text-center bg-black" >
+      {{ new Date().getFullYear() }} — <strong>Blogrr</strong>
+    </footer>
+  </v-app>
+
+</template>
